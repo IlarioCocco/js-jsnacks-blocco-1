cf558e393697a6d814ea7044f86cc8aab596c8d3
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-26 08:39:38 +0200

    commit

--- snack-5/js/scripts.js
+++ snack-5/js/scripts.js
@@ -1,4 +1,3 @@
-
 // Crea un array vuoto.Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
 
 // Crea un array vuoto, 
@@ -8,7 +7,7 @@ var containerNumeri = [];
 // se è dispari inseriscilo nell’array
 for (var i = 0; i < 6; i++) {
     var numero = prompt("inserisci il numero");
-    var resto = numero % 2;
+    var resto = numero % 1;
     containerNumeri.push(numero);
 }
 console.log(containerNumeri);
